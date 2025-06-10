432aa663725b9678560b1ca32ecfae95f4b271c8
maidongthuc <chechanh2003@gmail.com>
2025-06-10 20:37:37 +0700

    add sql

--- api/db_connection.py
+++ api/db_connection.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import pymysql
 db_config = {
     "host": "156.67.222.52",       # Địa chỉ máy chủ MySQL
     "user": "u238186000_chechanh",   # Tên người dùng MySQL
@@ -10,15 +10,15 @@ db_config = {
 # Tạo hàm kết nối đến MySQL
 def connect_to_database():
     try:
-        connection = MySQLdb.connect(
+        connection = pymysql.connect(
             host=db_config["host"],
             user=db_config["user"],
-            passwd=db_config["passwd"],
-            db=db_config["db"],
-            port=db_config["port"]    # Thêm tham số port
+            password=db_config["passwd"],
+            database=db_config["db"],
+            port=db_config["port"]
         )
         print("Database connection successful.")  # Thông báo khi kết nối thành công
         return connection
-    except MySQLdb.Error as e:
+    except pymysql.MySQLError as e:
         print(f"Error connecting to MySQL: {e}")  # Thông báo lỗi nếu kết nối thất bại
         return None
