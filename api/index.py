1a7adb2dfdab6f968d1b698c1c56e9bea4083f22
maidongthuc <chechanh2003@gmail.com>
2025-06-10 20:39:40 +0700

    add sql

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-from db_connection import connect_to_database
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,13 +7,3 @@ CORS(app)
 @app.route('/')
 def home():
     return 'Chào mừng bạn đến với Flask!'
-
-# Route có tham số tên, hiển thị lời chào cá nhân hóa
-@app.route('/hello')
-def greet():
-    conn = connect_to_database()
-    if conn:
-        conn.close()  # Close the connection after testing
-        return "Database connection successful!"
-    else:
-        return "Database connection failed!", 500
